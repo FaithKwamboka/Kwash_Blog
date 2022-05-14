a3d38d8eb720c029f9deff15ce76c92626bffa09
FaithKwamboka <okongofaith3@gmail.com>
2022-05-14 08:51:30 +0300

    Create forms classes

---
+++ app/main/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField, SelectField
+from wtforms.validators import DataRequired, Email
+
+
+class BlogForm(FlaskForm):
+
+    title = StringField('Author', validators=[DataRequired()])
+    post = TextAreaField('Blog', validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Let us know you better',validators = [DataRequired()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+
+class Vote(FlaskForm):
+    submit = SelectField('Like')
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Bio', validators=[DataRequired()])
+    submit = SubmitField('Post')
+
