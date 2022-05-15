eac20ca93e52698078b36fc51add05a3b5180707
FaithKwamboka <okongofaith3@gmail.com>
2022-05-15 18:44:51 +0300

    Deployment

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ class Config:
     General configuration parent class
     '''
     QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kwamboka:kwamboka@localhost/blog'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kwamboka:kwamboka@localhost/blog'
+    SQLALCHEMY_DATABASE_URI ='postgres://lrvenlerclxhff:799f028a5f2d666bcfedcebd5a0abf167f8f1a3df5f837b0a7d9aa14a40f6bf8@ec2-54-165-184-219.compute-1.amazonaws.com:5432/d3elql7m8a7919'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
@@ -22,7 +23,13 @@ class TestConfig(Config):
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+        uri = os.getenv('DATABASE_URL')
+        if uri and uri.startswith('postgres://'):
+            uri = uri.replace('postgres://', 'postgresql://', 1)
+            
+            SQLALCHEMY_DATABASE_URI=uri
+        
 
 
 class DevConfig(Config):
